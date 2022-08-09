6f0c5df0d2558956ed549ba475375e438d874788
clementef <clementef2000@gmail.com>
2022-08-09 13:39:32 -0700

    fish and starship configs

---
+++ Fish/config.fish
@@ -0,0 +1,40 @@
+#RESET PATH
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_path
+
+#UNIVERSAL VARS
+set -Ux PF_INFO "ascii title os host kernel shell memory palette"
+set -Ux TERM alacritty
+
+#CLEAR GREETING
+set fish_greeting
+
+#SET VI BINDINGS
+function fish_user_key_bindings
+ fish_vi_key_bindings
+end
+
+#ON STARTUP
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias config="vim ~/.config/fish/config.fish"
+    alias config_starship="vim ~/.config/starship.toml"
+    alias config_alacritty="vim ~/.config/alacritty/alacritty.yml"
+    alias config_vim="vim ~/.vimrc"
+    alias c="mc"
+    alias py="python3" 
+    alias godesk="cd ~/Desktop/"
+    alias gogit="cd ~/Git/"
+    alias goconfig="cd ~/.config"
+    alias godoc="cd ~/Documents/"
+    alias godown="cd ~/Downloads/"
+    alias gopics="cd ~/Pictures/"
+    alias o="open"
+    alias p="pfetch"
+    alias v="vim"
+    alias pico="/Applications/PICO-8.app/Contents/MacOS/pico8 -run"
+    pfetch
+end
+
+#SET STARSHIP PROMPT
+starship init fish | source
